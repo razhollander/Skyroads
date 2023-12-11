93e2e57499c79fcb289a8b85d250aa05eb041344
razhollander <50023823+razhollander@users.noreply.github.com>
2023-12-11 08:39:50 +0200

    time working

--- Assets/CoreDomain/GameDomain/GameStateDomain/MainGameDomain/Scripts/Modules/TimePlaying/TimePlayingModule.cs
+++ Assets/CoreDomain/GameDomain/GameStateDomain/MainGameDomain/Scripts/Modules/TimePlaying/TimePlayingModule.cs
@@ -2,46 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using CoreDomain.Services;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using UnityEngine;
 
-public class TimePlayingModule : ITimePlayingModule
+public class TimePlayingModule : ITimePlayingModule, IUpdatable
 {
+    private int TimePlaying => Mathf.FloorToInt(_timePlaying);
+
     private readonly TimePlayingChangedCommand.Factory _timePlayingChangedCommand;
-    private int _timePlaying;
+    private readonly IUpdateSubscriptionService _updateSubscriptionService;
+    private float _timePlaying;
+    private int _prevTimePlaying;
     private IDisposable _timerCoroutine;
     private CancellationToken _timerToken;
     private CancellationTokenSource _cancellationToken;
 
-    public TimePlayingModule(TimePlayingChangedCommand.Factory timePlayingChangedCommand)
+    public TimePlayingModule(TimePlayingChangedCommand.Factory timePlayingChangedCommand, IUpdateSubscriptionService updateSubscriptionService)
     {
         _timePlayingChangedCommand = timePlayingChangedCommand;
+        _updateSubscriptionService = updateSubscriptionService;
     }
     public void StartTimer()
     {
-        _cancellationToken = new CancellationTokenSource();
-        StartTimerTask(_cancellationToken).Forget();
-
-        //_timer = Observable.Interval(TimeSpan.FromMilliseconds(1000)).Subscribe(x =>
-        //{
-        //    _timePlayingChangedCommand.Create((int) x).Execute();
-        //});
-    }
-
-    private async UniTaskVoid StartTimerTask(CancellationTokenSource cancellationToken)
-    {
-        while (true)
-        {
-            await UniTask.Delay(1000, cancellationToken: cancellationToken.Token);
-            _timePlaying++;
-            _timePlayingChangedCommand.Create(_timePlaying).Execute();
-        }
+        _timePlaying = 0;
+        _prevTimePlaying = 0;
+        _updateSubscriptionService.RegisterUpdatable(this);
     }
 
     public void StopTimer()
     {
-        _cancellationToken.Cancel();
-        _timePlaying = 0;
+        _updateSubscriptionService.UnregisterUpdatable(this);
+    }
+
+    public void ManagedUpdate()
+    {
+        _timePlaying += Time.deltaTime;
+        var currentTimePlaying = Mathf.FloorToInt(_timePlaying);
+        
+        if (currentTimePlaying != _prevTimePlaying)
+        {
+            _prevTimePlaying = currentTimePlaying;
+            _timePlayingChangedCommand.Create(currentTimePlaying).Execute();
+        }
     }
 }
